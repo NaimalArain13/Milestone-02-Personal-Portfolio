8a6cb2f30cf0079a17b4777eb1e11330158f9dcb
NaimalArain13 <naimalarain13@gmail.com>
2025-10-05 19:00:52 +0500

    updated format

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -5,7 +5,7 @@ export default function Project() {
   const projects = [
 
     {
-      img: "/room.png",
+      img: "/room.jpg",
       title: "Room Matcher AI",
       description: "An AI powered room matcher multi-agent System.",
       route: "https://room-matcher-ai-rosy.vercel.app/",
